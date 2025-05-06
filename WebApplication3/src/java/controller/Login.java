bddfc586d8d39d954388a4358d24c12f612d2e6c
TanThanh69 <th1592004@gmail.com>
2025-05-06 19:15:47 +0700

    Add files via upload

---
+++ WebApplication3/src/java/controller/Login.java
@@ -0,0 +1,54 @@
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Login</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        response.setContentType("text/html;charset=UTF-8");      
+        
+            String userName = request.getParameter("user");
+            String password = request.getParameter("pass");
+            if(userName.equals("admin") && password.equals("123")){
+                pw.println("login succesfull");
+            } else{
+                pw.println("tài khoản, mật khẩu nhập còn sai thì sao mà chọn đúng người được!!!");
+            }
+        
+    }
+
+}
